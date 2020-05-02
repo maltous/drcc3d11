24545902854c580f24d1b66101c78d77cd75a080
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-02 19:36:48 +0300

    drcc3d11

---
+++ story_content/user.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6EOpwEe0mv2":
+        Script1();
+        break;
+      case "6aVDzIYOGrn":
+        Script2();
+        break;
+      case "6abij6ffVJZ":
+        Script3();
+        break;
+      case "6kTUh0ztlu8":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
